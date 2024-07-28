2d3ea0c79519b57df545ebcd358a1afd1b105ca0
Vaquos <furqonotong@gmail.com>
2024-07-28 23:20:30 +0700

    learn dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello("Samsudin"));
+
+  var product = Product();
+  var customer = Customer();
+  var category = Categoty();
+}
